27cc0f7c346a976b0cc3f1cf5401f4a5ce0e6e01
Vox Humana <jonprine@gmail.com>
2020-08-14 12:35:19 -0500

    added path and use

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.urlencoded( { extended: true }));
+app.use(express.json());
+
+// route to index
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+
+// route to notes
+
+app.get('notes.html', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/notes.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-})
