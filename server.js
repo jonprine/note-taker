82b98ec7601af5d4860996fa9c770512f2ccc6d7
Vox Humana <jonprine@gmail.com>
2020-08-14 12:59:50 -0500

    fixed route to notes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 // route to notes
 
-app.get('notes.html', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 })
 
