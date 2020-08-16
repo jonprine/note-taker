ded05d93251fa16c64ac547cb4c2f259ea1a4122
Vox Humana <jonprine@gmail.com>
2020-08-16 14:07:05 -0500

    added get for json

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const { notes } = require('./db/db.json');
+const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(express.urlencoded( { extended: true }));
 app.use(express.json());
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+app.post('/api/notes', (req, res) => {
+    
+
+})
 
 // route to index
 
