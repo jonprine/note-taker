c275d9aeb72ed6d7ec6a924779747f5895e8013d
Vox Humana <jonprine@gmail.com>
2020-08-16 20:16:28 -0500

    added post and stringify parse

--- server.js
+++ server.js
@@ -5,19 +5,22 @@ const app = express();
 const { notes } = require('./db/db.json');
 const fs = require('fs');
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 
 app.use(express.urlencoded( { extended: true }));
 app.use(express.json());
 
+
+
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json'));
+    fs.readFile('./db/db.json', 'utf8', function(err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
 });
-
-app.post('/api/notes', (req, res) => {
-    
-
-})
 
 // route to index
 
@@ -30,6 +33,24 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 })
+
+app.post('/api/notes', (req, res) => {
+    
+    fs. readFile('./db/db.json', 'utf8', function (err, data) {
+        if (err) throw err;
+
+        let note = JSON.parse(data)
+        req.body.id = note.length + 1
+        note.push(req.body)
+        res.send()
+
+        fs.writeFile('./db/db.json', JSON.stringify(note), function(err) {
+            if (err) throw err;
+            console.log('Note added');
+        })
+    })
+
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
